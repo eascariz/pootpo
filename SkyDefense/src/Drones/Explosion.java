3d7914f41d22a42cb7f2f4acf7def4dc62dffad7
TomasRey1 <ulisesruizr3@gmail.com>
2026-06-04 21:18:03 -0300

    Update Explosion.java

--- SkyDefense/src/Drones/Explosion.java
+++ SkyDefense/src/Drones/Explosion.java
@@ -16,11 +16,22 @@ public class Explosion {
 		this.radio = radio;
 	}
 
-	public int calcularDistancia(Avion avion) {
-		
+	public double calcularDistancia(Avion avion) {
+		int[] posicion = avion.obtenerPosicion();
+		int posAvionX = posicion[0];
+		int posAvionY = posicion[1];
+		return Math.sqrt(Math.pow((posAvionX - posicionX), 2) + Math.pow((posAvionY - posicionY), 2));
 	}
 	
 	public void mostrar() {
 		
 	}
 }
+
+/*
+Explosion — creada cuando detona un misil.
+
+Explosion(int posicionX, int posicionY, int radio) — inicializa posición y radio.
+calcularDistancia(Avion avion) — calcula la distancia al avión usando la fórmula euclidiana.
+mostrar() — muestra la explosión en pantalla.
+*/
